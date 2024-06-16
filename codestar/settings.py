04dec378a971999de2a0a25690e3ac0af6142ece
Michael Baker <michael.w.barker@hotmail.co.uk>
2024-06-16 10:32:52 +0000

    Create new 'blog' app with view

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-($48a_so#g=px_kqg(v0%(z9$7)no#g(i3a616xiyb=l25h+2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-mwbark-djangociblog-58hirnmlhgo.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
